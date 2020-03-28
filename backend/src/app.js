a0ecafa2a291733fd303276f8f1763859a395453
João V <silva.joao1@outlook.com.br>
2020-03-28 13:59:11 -0300

    Adicionado comentários

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,20 @@ const cors = require('cors');
 const { errors } = require('celebrate');
 const routes = require('./routes');
 
-//Framework usado para lidar com as requisições HTTP
+//Express é um Framework usado para lidar com as requisições HTTP
 const app = express();
 
-//Módulo de segurança da aplicação
+/*
+ * CORS = Cross-Origin Resource Sharing (Compartilhamento de recursos com origens diferentes)
+ * Basicamente é um mecanismo que permite a solicitação de um recurso que tenha origem diferente
+ * Usado para permitir comunicação com o front-end
+*/
 app.use(cors());
 
 //Middleware responsável por converter as requisições HTTP em objetos JSON
 app.use(express.json());
 
-//Sistema de rotas do back-end*/
+//Sistema de rotas do back-end
 app.use(routes);
 
 //Middleware que irá lidar com o erros de validação vindo do celebrate
